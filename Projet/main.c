4873aad42763df87286e560dbd1eed2a7522a0cd
Fred <frederic.Flaceliere@gmail.com>
2019-11-04 22:01:19 +0100

    ajout connexion bdd

--- Projet/main.c
+++ Projet/main.c
@@ -6,14 +6,35 @@
 //  Copyright © 2019 Frédéric FLACELIERE. All rights reserved.
 //
 
+//#include "connectionBDD.h"
+
 #include <stdio.h>
 #include <stdlib.h>
-//#include <mysql.h>
+
+#include <mysql.h>
+
 
 int main(int argc, const char * argv[]) {
+    MYSQL mysql;
+    mysql_init(&mysql);
+    
     // insert code here...
     printf("Hello, World!\n");
     printf("Hello, World a seconde time!\n");
     printf("Hello, World a third time!\n");
+    
+
+
+    mysql_options(&mysql,MYSQL_READ_DEFAULT_GROUP,"option");
+
+    if(mysql_real_connect(&mysql,"54.37.153.32","cproject","Fr3dCl3m","mainCBase",3306,NULL,0))
+    {
+        printf("DONE ! :D");
+        mysql_close(&mysql);
+    }
+    else
+    {
+        printf("Une erreur s'est produite lors de la connexion à la BDD!");
+    }
     return 0;
 }
