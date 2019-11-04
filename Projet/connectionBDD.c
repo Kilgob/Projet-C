97e362afa9274f6841fe26794b15b1ffd179fc0c
Fred <frederic.Flaceliere@gmail.com>
2019-11-04 17:10:13 +0100

    new master

--- Projet/connectionBDD.c
+++
@@ -1,9 +0,0 @@
-//
-//  connectionBDD.c
-//  Projet
-//
-//  Created by Frédéric FLACELIERE on 04/11/2019.
-//  Copyright © 2019 Frédéric FLACELIERE. All rights reserved.
-//
-
-#include "connectionBDD.h"
